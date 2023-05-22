079d4f4c55e7dfc1fb4f6cd4e1518adbe59ff9a2
AntohaBr <brelanton@gmail.com>
2023-05-22 19:17:26 +0300

    Updates

---
+++ precache-manifest.82c6ed2bed48d670dbef01ccb5a6d4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f66e8ad0f1508884b8c74cf8c762d76",
+    "url": "/AntohaBr/Todolist/index.html"
+  },
+  {
+    "revision": "195eb896c532e28f4f7a",
+    "url": "/AntohaBr/Todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/AntohaBr/Todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/AntohaBr/Todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195eb896c532e28f4f7a",
+    "url": "/AntohaBr/Todolist/static/js/main.1f904299.chunk.js"
+  },
+  {
+    "revision": "edb16b0a1f989d496813",
+    "url": "/AntohaBr/Todolist/static/js/runtime-main.bc53df9e.js"
+  }
+]);
